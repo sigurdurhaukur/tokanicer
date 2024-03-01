5ea5292adb3b65469d0f76fc90e80347aeb1c39a
sigurdurhaukur <56935611+sigurdurhaukur@users.noreply.github.com>
2024-03-01 10:46:54 +0000

    better data_loader

--- data_loader.sh
+++ data_loader.sh
@@ -7,4 +7,20 @@ fi
 
 cd raw_data
 
+# 3-4 GB after unzipping
 curl --remote-name-all https://repository.clarin.is/repository/xmlui/bitstream/handle/20.500.12537/236{/IGC-News1-22.10.TEI.zip}
+
+# ask for permission to unzip
+
+read -p "Do you want to unzip the files? (y/n): " answer
+if [[ $answer == "y" ]]; then
+  unzip \*.zip
+fi
+
+read -p "Do you want to run the preprocessing script? (y/n): " answer
+if [[ $answer == "y" ]]; then
+  cd ..
+  python3 preprocessing.py
+fi
+
+echo "Data has been downloaded and preprocessed. You can now run the main script."
